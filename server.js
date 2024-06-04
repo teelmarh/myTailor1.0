37e4822ff6023a0881246bb859216ebfd1582031
teelmarh <101597548+teelmarh@users.noreply.github.com>
2024-06-04 15:33:21 +0100

    first commit

--- server.js
+++ server.js
@@ -1,31 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 5000;
 const db = require("./config/db");
 const jwt = require("jsonwebtoken");
+const verifyToken = require("./verifyToken");
+const port = 5000;
 require("dotenv");
 
 app.use(cors());
 app.use(express.json());
-
-// Middleware for verifying JWT tokens
-const verifyToken = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ message: "Unauthorized access" });
-    }
-
-    const token = authHeader.split(" ")[1]; // Extract token from "Bearer " prefix
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // Attach decoded user information to the request
-        next(); // Allow access to the protected route
-    } catch (err) {
-        return res.status(401).json({ message: "Session expired" }); // Handle expired token
-    }
-};
 
 //routes
 const signUpRouter = require("./Api/account/signUp.js");
---
+++ verifyToken.js
@@ -0,0 +1,19 @@
+// Middleware for verifying JWT tokens
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Unauthorized access" });
+    }
+
+    const token = authHeader.split(" ")[1]; // Extract token from "Bearer " prefix
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded; // Attach decoded user information to the request
+        next(); // Allow access to the protected route
+    } catch (err) {
+        return res.status(401).json({ message: "Session expired" }); // Handle expired token
+    }
+};
+
+module.exports = verifyToken;
